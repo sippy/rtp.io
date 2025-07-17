786e286e66c15e2abf22cdc9762d0ca0987bd0c2
Maksym Sobolyev <sobomax@sippysoft.com>
2025-07-17 12:37:47 -0700

    Make this 1.0.

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ def main():
 
     setup(
         name='rtp.io',
-        version='0.9',
+        version='1.0',
         description='Fast RTPProxy-like RTP handling/processing engine.',
         ext_modules=ext_modules,
         cmdclass={
