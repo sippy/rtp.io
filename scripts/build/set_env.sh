28922f3dea4432b07293f2bdd8edfe36f445453c
Maksym Sobolyev <sobomax@sippysoft.com>
2025-10-25 08:02:20 -0700

    Switch from using CCACHE_COMPILERCHECK=content to CCACHE_COMPILERCHECK=string based on the has of the compiler version output.

--- scripts/build/set_env.sh
+++ scripts/build/set_env.sh
@@ -2,6 +2,25 @@
 
 set -e
 
+get_cc_ver() {
+  CC_v="`${CC} -v 2>&1`"
+  if [ "${?}" -ne 0 -o ! -n "${CC_v}" ]
+  then
+    exit 1
+  fi
+  CC_dm="`${CC} -dumpmachine 2>&1`"
+  if [ "${?}" -ne 0 -o ! -n "${CC_dm}" ]
+  then
+    exit 1
+  fi
+  CC_dv="`${CC} -dumpversion 2>&1`"
+  if [ "${?}" -ne 0 -o ! -n "${CC_dv}" ]
+  then
+    exit 1
+  fi
+  echo "${CC_v}${CC_dm}${CC_dv}" | md5sum | awk '{print $1}'
+}
+
 platformopts() {
   case "${TARGETPLATFORM}" in
   linux/arm64)
@@ -21,6 +40,12 @@ platformopts() {
   then
     echo "SRTP_LIBS=-L/opt/rh/gcc-toolset-14/root/usr/lib/gcc/i686-redhat-linux/14 -l:libatomic.a"
   fi
+  CC_VER="`get_cc_ver`"
+  if [ "${?}" -ne 0 -o ! -n "${CC_VER}" -o "${CC_VER}" = "68b329da9893e34099c7d8ad5cb9c940" ]
+  then
+    exit 1
+  fi
+  echo "CCACHE_COMPILERCHECK=string:${CC_VER}"
   echo "SAVE_SPACE=yes"
 }
 
